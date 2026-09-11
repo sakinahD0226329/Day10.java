0421d944be67bf2f9a72a0ad5a13f0704ede88b5
sakinahD0226329 <sakinaaaa28@gmail.com>
2026-09-11 23:14:46 +0800

    Update Day10.java
    ;)

--- Day10.java
+++ Day10.java
@@ -8,7 +8,9 @@
           System.out.println("Int   : " + Integer.MIN_VALUE + " sampai " + Integer.MAX_VALUE);
           System.out.println("Long  : " + Long.MIN_VALUE + " sampai " + Long.MAX_VALUE);
           System.out.println("Float : " + Float.MIN_VALUE + " sampai " + Float.MAX_VALUE);
-          System.out.println("Duble : " + Double.MIN_VALUE + " sampai " + Double.MAX_VALUE);
-
+          System.out.println("Double : " + Double.MIN_VALUE + " sampai " + Double.MAX_VALUE);
+          System.out.println("Char   : " + (int) Character.MIN_VALUE + " sampai " + (int)  Character.MAX_VALUE);
+          System.out.println("Boolean : true atau false");
+                             
       }
   }
