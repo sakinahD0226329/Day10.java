6747ba0cebe4bae26d50087e7330e2df13d579b4
sakinahD0226329 <sakinaaaa28@gmail.com>
2026-09-11 22:28:21 +0800

    Create Day10.java
    done

---
+++ Day10.java
@@ -0,0 +1,14 @@
+  public class BatasTipeData {
+      public static void main(string[] args) {
+        
+          System.out.println("Batas Minimal dan Maksimal Tipe Data")
+
+          System.out.println("Byte  : " + Byte.MIN_VALUE + " sampai " + Byte.MAX_VALUE);
+          System.out.println("Short : " + Short.MIN_VALUE + " sampai " + Short.MAX_VALUE);
+          System.out.println("Int   : " + Integer.MIN_VALUE + " sampai " + Integer.MAX_VALUE);
+          System.out.println("Long  : " + Long.MIN_VALUE + " sampai " + Long.MAX_VALUE);
+          System.out.println("Float : " + Float.MIN_VALUE + " sampai " + Float.MAX_VALUE);
+          System.out.println("Duble : " + Double.MIN_VALUE + " sampai " + Double.MAX_VALUE);
+
+      }
+  }
